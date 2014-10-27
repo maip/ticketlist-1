a86c883a4da65f375fc2a54d8785883c28585c7c
Melissa Ip <nokami.hybrids@gmail.com>
2014-10-26 21:20:46 -0700

    make settings page edit user with avatar upload and display

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   
   has_many :tickets
+
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/missing.png"
+  do_not_validate_attachment_file_type :avatar
 end
