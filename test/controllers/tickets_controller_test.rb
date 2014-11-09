0f9a750de108c570ebcfd6b60a6304cdc836f233
Melissa Ip <nokami.hybrids@gmail.com>
2014-11-08 23:15:42 -0700

    add readme, add tests

--- test/controllers/tickets_controller_test.rb
+++ test/controllers/tickets_controller_test.rb
@@ -9,6 +9,7 @@ class TicketsControllerTest < ActionController::TestCase
     get :index
     assert_response :success
     assert_not_nil assigns(:tickets)
+    # check sorted by datetime
   end
 
   test "should get new" do
@@ -17,39 +18,17 @@ class TicketsControllerTest < ActionController::TestCase
   end
 
   test "should create ticket" do
+    event = Event.find(@ticket.event_id)
+    before_available_tickets = event.available_tickets
     assert_difference('Ticket.count') do
       post :create, ticket: { event_id: @ticket.event_id, user_id: @ticket.user_id,  num_booked: @ticket.num_booked }
     end
+    assert(event.available_tickets == before_available_tickets-@ticket.num_booked, 'Incorrect subtraction of tickets booked from available tickets' )
     assert_redirected_to ticket_path(assigns(:ticket))
   end
 
   test "should show ticket" do
     get :show, id: @ticket
-    assert_response :success
-  end
-
-
-  test "should show tickets by time" do
-    get :index, id: @etickets_controller
-    assert(@ticket == @ticket.sort_by &:datetime, 'sorts tickets by time')
-  end
-
-
-  test "updating available tickets after booking tickets" do
-    get :ticket, id: @ticket
-   assert(@ticket.available_tickets > @ticket.total_tickets, 'The number of available tickets is greater than total tickets' )
-  end
-
-    test "book tickets function" do
-   assert_difference('ticket.count') do
-      post :book, ticket: { available_tickets: @ticket.available_tickets, user_id: @ticket.user_id, datetime: @ticket.datetime, event_type: @ticket.event_type, title: @ticket.title, total_tickets: @ticket.total_tickets, venue: @ticket.venue }
-      patch :update, id: @ticket, ticket: { available_tickets: @ticket.available_tickets, user_id: @ticket.user_id, datetime: @ticket.datetime, ticket_type: @ticket.ticket_type, title: @ticket.title, total_tickets: @ticket.total_tickets, venue: @ticket.venue }
-    assert_redirected_to ticket_path(assigns(:ticket))
-  end
-end
-
-  test "should get edit" do
-    get :edit, id: @ticket
     assert_response :success
   end
 
