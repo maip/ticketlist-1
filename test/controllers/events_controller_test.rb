226d017f4d01fe536cb11a6de2fdbafdd1a0cd86
sichunai <sichunai@sichuns-air.mobile.asu.edu>
2014-11-08 20:07:13 -0700

    unit tests...

--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -18,10 +18,10 @@ class EventsControllerTest < ActionController::TestCase
 
   test "should create event" do
     assert_difference('Event.count') do
-      post :create, event: { available_tickets: @event.available_tickets, date: @event.date, event_type: @event.event_type, time: @event.time, title: @event.title, total_tickets: @event.total_tickets, venue: @event.venue }
+      post :create, event: { available_tickets: @event.available_tickets, user_id: @event.user_id, date: @event.date, event_type: @event.event_type, time: @event.time, title: @event.title, total_tickets: @event.total_tickets, venue: @event.venue }
     end
 
-    assert (@event.available_tickets <= @event.total_tickets [ "Available tickets is not less than or equal to total tickets"])
+    assert (if @event.available_tickets <= @event.total_tickets,  'Available tickets is not less than or equal to total tickets')
     
     assert_redirected_to event_path(assigns(:event))
   end
@@ -31,13 +31,37 @@ class EventsControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "should show events by time" do
+    get :index, id: @events_controller
+    assert(if @events = @events.sort_by &:datetime, 'sorts events by time')
+  end
+
+  test "book tickets function" do
+   assert_difference('Event.count') do
+      post :create, event: { available_tickets: @event.available_tickets, user_id: @event.user_id, date: @event.date, event_type: @event.event_type, time: @event.time, title: @event.title, total_tickets: @event.total_tickets, venue: @event.venue }
+      patch :update, id: @event, event: { available_tickets: @event.available_tickets, user_id: @event.user_id, date: @event.date, event_type: @event.event_type, time: @event.time, title: @event.title, total_tickets: @event.total_tickets, venue: @event.venue }
+    assert_redirected_to event_path(assigns(:event))
+  end
+end
+
+  test "updating available tickets after booking tickets" do
+    get :ticket, id: @event
+   assert (if @event.available_tickets > @event.total_tickets, 'The number of available tickets is greater than total tickets' )
+  end
+
+  test "cant create events in the past" do
+    get :create, id:@events_controller
+    assert (if @event.datetime < DateTime.current, 'Cannot create an event in the past')
+  end
+
+
   test "should get edit" do
     get :edit, id: @event
     assert_response :success
   end
 
   test "should update event" do
-    patch :update, id: @event, event: { available_tickets: @event.available_tickets, date: @event.date, event_type: @event.event_type, time: @event.time, title: @event.title, total_tickets: @event.total_tickets, venue: @event.venue }
+    patch :update, id: @event, event: { available_tickets: @event.available_tickets, user_id: @event.user_id, date: @event.date, event_type: @event.event_type, time: @event.time, title: @event.title, total_tickets: @event.total_tickets, venue: @event.venue }
     assert_redirected_to event_path(assigns(:event))
   end
 
