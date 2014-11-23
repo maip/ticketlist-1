43e60b660a3d9a989385a5d03caca351a858ae36
Melissa Ip <nokami.hybrids@gmail.com>
2014-11-23 13:04:11 -0700

    fix unit tests

--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -4,51 +4,29 @@ class EventsControllerTest < ActionController::TestCase
   setup do
     @event = events(:one)
     @future_event = events(:two)
-    # @past_event = events(:three)
-  end
 
-  test "should sign in user" do
-    user = User.create!({
+    @user = User.create!({
       email: "test@gmail.com",
       password: "password"
     })
-    sign_in user
-    assert_response :success
-    assert @controller.instance_variable_set(:"@current_user", true)
+    sign_in @user
   end
+
+  # test "should sign in user" do
+  #   user = User.create!({
+  #     email: "test@gmail.com",
+  #     password: "password"
+  #   })
+  #   sign_in user
+  #   assert_response :success
+  #   assert @controller.instance_variable_set(:"@current_user", true)
+  # end
 
   test "should get index" do
-    user = User.create!({
-      email: "test@gmail.com",
-      password: "password"
-    })
-    sign_in user
-
     get :index
     assert_response :success
     assert_not_nil assigns(:events)
-
-    event = Event.create({
-      available_tickets: @event.available_tickets,
-      datetime: @event.datetime,
-      event_type: @event.event_type,
-      title: @event.title,
-      total_tickets: @event.total_tickets,
-      venue: @event.venue,
-      user_id: user.id,
-      photo: fixture_file_upload('placeholder.png', 'image/png')
-    })
-    future_event = Event.create({ available_tickets: @future_event.available_tickets,
-      datetime: @future_event.datetime,
-      event_type: @future_event.event_type,
-      title: @future_event.title,
-      total_tickets: @future_event.total_tickets,
-      venue: @future_event.venue,
-      user_id: user.id,
-      photo: fixture_file_upload('placeholder.png', 'image/png')
-    })
-    all = Event.all
-    assert_operator(all.index(event), :<, all.index(future_event), 'Events not sorted by datetime')
+    assert_operator(assigns(:events).index(@event), :<, assigns(:events).index(@future_event), 'Events not sorted by datetime')
   end
 
   test "should get new" do
@@ -57,12 +35,6 @@ class EventsControllerTest < ActionController::TestCase
   end
 
   test "should create event" do
-    user = User.create!({
-      email: "test@gmail.com",
-      password: "password"
-    })
-    sign_in user
-
     assert(@event.available_tickets <= @event.total_tickets, 'Available tickets is greater than total tickets')
     assert(@event.datetime >= DateTime.current, "Event #{@event.datetime} is in the past")
     assert_difference('Event.count') do
@@ -73,7 +45,7 @@ class EventsControllerTest < ActionController::TestCase
                       title: @event.title,
                       total_tickets: @event.total_tickets,
                       venue: @event.venue,
-                      #user_id: user.id,
+                      user_id: @user.id,
                       photo: fixture_file_upload('placeholder.png', 'image/png')
                     }
     end
@@ -94,7 +66,7 @@ class EventsControllerTest < ActionController::TestCase
                                  title: @event.title,
                                  total_tickets: @event.total_tickets,
                                  venue: @event.venue,
-                                 # user_id: user.id,
+                                 user_id: @user.id,
                                  photo: fixture_file_upload('placeholder.png', 'image/png')
                                }
     assert(@event.available_tickets <= @event.total_tickets, 'Available tickets is greater than total tickets')
@@ -106,6 +78,10 @@ class EventsControllerTest < ActionController::TestCase
     assert_difference('Event.count', -1) do
       delete :destroy, id: @event
     end
+
+    # assert delete tickets with matching event id
+    tickets = Ticket.where(event_id: @event.id)
+    assert(tickets.empty?, "Did not delete tickets for this event")
 
     assert_redirected_to events_path
   end
--- test/controllers/tickets_controller_test.rb
+++ test/controllers/tickets_controller_test.rb
@@ -4,21 +4,18 @@ class TicketsControllerTest < ActionController::TestCase
   setup do
     @ticket = tickets(:one)
     @event = events(:one)
-    @user = users(:one)
-  end
 
-  test "should get index" do
-    user = User.create!({
+    @user = User.create!({
       email: "test@gmail.com",
       password: "password"
     })
-    sign_in user
-    
+    sign_in @user
+  end
+
+  test "should get index" do
     get :index
     assert_response :success
     assert_not_nil assigns(:tickets)
-
-    # assert sorted by event datetime
   end
 
   test "should get new" do
@@ -27,34 +24,35 @@ class TicketsControllerTest < ActionController::TestCase
   end
 
   test "should create ticket" do
-    user = User.create!({
-      email: "test@gmail.com",
-      password: "password"
-    })
-    sign_in user
-
-    event = Event.create({
-      available_tickets: @event.available_tickets,
-      datetime: @event.datetime,
-      event_type: @event.event_type,
-      title: @event.title,
-      total_tickets: @event.total_tickets,
-      venue: @event.venue,
-        user_id: user.id,
-      photo: fixture_file_upload('placeholder.png', 'image/png')
-    })
     assert_difference('Ticket.count') do
-      post :create, ticket: { event_id: event.id, user_id: user.id, num_booked: @ticket.num_booked }
+      post :create, ticket: { event_id: @event.id, user_id: @user.id, num_booked: @ticket.num_booked }
     end
     assert_redirected_to ticket_path(assigns(:ticket))
     
-    ticket = Ticket.create({
-      event_id: event.id,
-      user_id: user.id,
-      num_booked: @ticket.num_booked
-    })
-    found_event = Event.find(ticket.event_id)
-    assert(found_event.available_tickets == @event.available_tickets-ticket.num_booked, "#{found_event.available_tickets} != #{@event.available_tickets} - #{ticket.num_booked}" )
+    updated_event = Event.find(@event)
+    assert(updated_event.available_tickets == @event.available_tickets-@ticket.num_booked,
+           "#{updated_event.available_tickets} != #{@event.available_tickets} - #{@ticket.num_booked}" ) 
+
+    assert_no_difference('Ticket.count') do
+      post :create, ticket: { event_id: @event.id, user_id: @user.id, num_booked: 1 }
+    end
+
+    existing_ticket = Ticket.find_by(event_id: @event.id, user_id: @user.id)
+    assert(existing_ticket.num_booked == @ticket.num_booked+1,
+           "Did not add booked to existing ticket #{existing_ticket.num_booked}.")   
+  end
+
+  test "should sort tickets by event datetime" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:tickets)
+
+    past_event = @event
+    assigns(:tickets).each do |ticket|
+      event = Event.find(ticket.event_id)
+      assert(event.datetime >= past_event.datetime, "Tickets not ordered by datetime.")
+      past_event = event
+    end
   end
 
   test "should show ticket" do
